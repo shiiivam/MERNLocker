6e8a37f01d1b486eb8580e46dae23123a85975e7
shivam sharma <shivam.cloud02@gmail.com>
2021-06-18 23:38:53 +0530

    added jwt auth

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,11 @@
+// JWT for generating auth token
+const jwt = require('jsonwebtoken');
+// Mongoose to connect with MDB
 const mongoose = require('mongoose');
+// bcryptjs to hash the password
+const bcrypt = require('bcryptjs');
+// Key to hash tokens
+const SECRET_KEY = process.env.SECRET_KEY; 
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -25,7 +32,37 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    tokens:[
+        {
+            token:{
+                type:String,
+                required:true
+            }
+        }
+     ]
 })
+
+// Hashing the password
+userSchema.pre('save',async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,12);
+        this.cpassword = await bcrypt.hash(this.cpassword,12);
+    }
+    next();
+});
+
+// Generating Token
+userSchema.methods.generateAuthToken = async function(){
+    try{
+        const token = jwt.sign({_id:this._id},process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    }catch(err){
+        console.log(err);
+    }
+
+}
 
 const USER = mongoose.model('USER',userSchema);
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Importing the model
 const User = require('../models/userSchema');
@@ -54,38 +56,50 @@ router.post('/register', async (req,res)=>{
         
         if(userExist){
             return res.status(422).json({error:"User already registered"});
-        }
-        
-        const user = new User({name, email, phone, work, password, cpassword});
+        }else if(password != cpassword){
+            return res.status(422).json({error:"Passwords doesn't match"});
+        }else{
+            const user = new User({name, email, phone, work, password, cpassword});
             
-        await user.save()    
+            const registeredUser = await user.save();    
 
-        res.status(201).json({message:"User registered successfully"})
+            res.status(201).json({message:`User registered successfully ${registeredUser}`});
+        }
     }catch(err){
-        res.status(500).json({message:"Can't register"})
+        res.status(500).json({error:`Can't register : ${err}`})
     }   
            
 })
 
 router.post('/signin',async (req,res)=>{
-    const {email, password} = req.body;
-
-    // Validating request
-    if(!email || !password){
-       return res.status(422).json({error:"Please fill all the details"});
-    }
+    
     try{
         const {email, password} = req.body;
 
         // Validating request
         if(!email || !password){
             return res.status(400).json({error:"Please fill all the details"});
-        }e33
-        const userLogin = await User.findOne({email:email})
-        if(!userLogin){
-            return res.status(422).json({error:"Invalid crendentials"});
+        }
+        
+        const userLogin = await User.findOne({email:email})        
+
+        if(userLogin){
+            // Comparing password
+            const isMatched = await bcrypt.compare(password, userLogin.password);
+            // To generate JWT on signing in
+            // const token = await userLogin.generateAuthToken();
+            // console.log(token);
+           
+            if(isMatched){
+                // To generate JWT on signing in
+                const token = await userLogin.generateAuthToken();
+                console.log(token);
+                return res.status(201).json({message:"user logged in successfully"});
+            }else{
+                return res.status(400).json({error:"Invalid Credentials"});
+            }
         }else{
-            return res.status(201).json({message:"user logged in successfully"});
+            return res.status(400).json({error:"Invalid Credentials"});
         }
     }catch(err){
         res.status(500).json({error:err})
